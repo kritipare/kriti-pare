8ccdbfce544b1df50732b01cd2b24668605a44a7
kritipare308 <kritipare308@gmail.com>
2024-09-06 16:03:39 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Banner from "./components/Banner";
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="w-full bg-gray-950 text-gray-300 px-4">
+      <div className="max-w-screen-xl mx-auto lg:pt-10">
+        <Banner />
+        {/* <Aboutme /> */}
+        {/* <Experience /> */}
+        {/* <Skills /> */}
+        {/* <Projects /> */}
+        {/* <Contact /> */}
+        {/* <Footer /> */}
+      </div>
+    </div>
+  )
+}
+
+export default App
